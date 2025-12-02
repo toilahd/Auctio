be7942cd28be5406d89aa96d366d05912733c5b3
mèo <trannguyenphuckhang12@gmail.com>
2025-12-02 11:13:51 +0700

    feat: add tailwind and remove old styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <h1>Heading h1</h1>
+      <h2>Heading h2</h2>
+      <h3>Heading h3</h3>
+      <h4>Heading h4</h4>
+      <h5>Heading h5</h5>
+      <h6>Heading h6</h6>
+      <p>
+        This is a paragraph of text to demonstrate the global styles applied to
+        paragraphs in the application. It should have a comfortable line height
+        and font size for readability.
       </p>
-    </>
-  )
+      <button>Click Me</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
